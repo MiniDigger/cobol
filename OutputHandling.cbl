e5d02be904022d84f5255030d2ddf0b9f6ff7743
MiniDigger <admin@minidigger.me>
2017-01-11 21:37:02 +0100

    fix matrix output

--- OutputHandling.cbl
+++ OutputHandling.cbl
@@ -17,12 +17,13 @@
        FILE SECTION.
            FD OUTPUTF
            DATA RECORD IS OUTPUT-FILE.
-           01 OUTPUT-FILE PIC X(100).
+           01 OUTPUT-FILE PIC X(200).
        WORKING-STORAGE SECTION.
            01 FILE-STATUS PIC XX.
            01 COUNTER PIC 9(4) COMP-3 VALUE 1.
            01 WS-RET-EW PIC S9(06)V9(16).
-           01 PRINT PIC X(100).
+           01 PRINT PIC X(200).
+           01 PRETTY-NUM PIC -Z(8)9.9999.
            01 I PIC 9(4).
            01 J PIC 9(4).
            01 P PIC 9(4).
@@ -32,29 +33,37 @@
            COPY "InputMatrix.cpy".
        PROCEDURE DIVISION USING ABBRUCH INPUT-VEKTOR MATRIX.
        MAIN-PROCEDURE.
-           MOVE RET-EW TO WS-RET-EW
-      *     MOVE WS-RET-EW TO PRINT
-
             OPEN OUTPUT OUTPUTF
             IF FILE-STATUS NOT = '00'
                THEN PERFORM HANDLE-ERROR STOP RUN
             END-IF
 
             WRITE OUTPUT-FILE FROM "=========="
-            WRITE OUTPUT-FILE FROM "Matrix:"
 
+            WRITE OUTPUT-FILE FROM "Matrix:"
             MOVE 1 TO I
             PERFORM UNTIL I > DIM-M
                MOVE 1 TO J
                MOVE 1 TO P
-               MOVE "" TO PRINT
+               MOVE SPACE TO PRINT
                PERFORM UNTIL J > DIM-M
-                  STRING ELEM(I,J) " " INTO PRINT WITH POINTER P
+                   MOVE ELEM(I,J) TO PRETTY-NUM
+                   STRING PRETTY-NUM " " INTO PRINT WITH POINTER P
+                  ADD 1 TO J
                END-PERFORM
+               ADD 1 TO I
             WRITE OUTPUT-FILE FROM PRINT
             END-PERFORM
 
             WRITE OUTPUT-FILE FROM "Start Vektor:"
+            MOVE 1 TO I
+            MOVE SPACE TO PRINT
+            PERFORM UNTIL I > DIM-I
+                MOVE XI(I) TO PRETTY-NUM
+                  STRING PRETTY-NUM " " INTO PRINT WITH POINTER P
+            END-PERFORM
+            WRITE OUTPUT-FILE FROM PRINT
+
             WRITE OUTPUT-FILE FROM "Erwartungswert:"
             MOVE "test" TO PRINT
             WRITE OUTPUT-FILE FROM PRINT
